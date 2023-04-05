c8e0b8b8e7a88a85810612a54b44d0259d898292
年輪 <nenrin.yearring@gmail.com>
2023-04-06 04:03:06 +0900

    add tailwind and rewrite page.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  mode: 'jit',
+  content: [
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
