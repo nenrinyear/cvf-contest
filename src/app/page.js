45f57f9cbe69dc67077bc533b99007c48a1a4306
年輪 <nenrin.yearring@gmail.com>
2023-04-08 03:36:28 +0900

    ios optimized

--- src/app/page.js
+++ src/app/page.js
@@ -13,31 +13,30 @@ export default function Home() {
     useEffect(() => {
         const video = document.getElementById('logo-mp4');
         video.play()
-            .then(() => {
-                console.log('Video is playing');
-            })
             .catch((error) => {
-                console.log('Video is not playing');
-                console.log(error);
+                const image = new Image();
+                image.src = '/CVF_static_icon.webp';
+                image.className = 'sm:w-1/4 w-full rounded-full';
+                video.insertAdjacentHTML('afterend', image.outerHTML);
+                video.remove();
             });
     }, []);
     return (
         <div className="top">
-            <header>
-                <div className="w-screen h-screen bg-black flex justify-center items-center">
-                    <video muted loop className="w-1/4 rounded-full" id='logo-mp4' >
+            <header className='w-screen h-screen bg-black'>
+                <div className="w-full sm:h-full h-2/3 flex justify-center items-center">
+                    <video muted loop className="sm:w-1/4 w-full rounded-full" id='logo-mp4' >
                         <source src="/CVF_motion.mp4" type="video/mp4" />
                     </video>
                 </div>
-                <script></script>
-                <div className="fixed bottom-0 mt-0 mr-0 mb-12 ml-12">
-                    <p className={`text-white font-extralight italic text-9xl select-none pt-2 pb-2 pr-0 pl-0 ${jost.className}`}>
+                <div className="absolute bottom-0 left-0 mt-0 mr-0 sm:mb-12 mb-8 sm:ml-12 ml-8 bg-transparent">
+                    <p className={`text-white font-extralight italic text-8xl select-none pt-2 pb-2 pr-0 pl-0 ${jost.className}`}>
                         CVF
                     </p>
                 </div>
             </header>
             <main>
-
+                <div className="h-screen">aaaaaaaaaaaaaa</div>
             </main>
         </div>
     )
