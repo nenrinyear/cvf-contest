e0b88fd0b771b6511a299e409dbbe77057f1831e
年輪 <nenrin.yearring@gmail.com>
2023-04-12 03:56:57 +0900

    removed tailwindcss from logo motion

--- src/logo-animation.js
+++ src/logo-animation.js
@@ -9,11 +9,11 @@ export default function LogoMovieAnimation({ className }) {
             .catch((error) => {
                 const image = new Image();
                 image.src = '/CVF_static_icon.webp';
-                image.className = 'sm:tw-w-1/4 tw-w-1/2 tw-rounded-full';
+                image.className = className;
                 video.insertAdjacentHTML('afterend', image.outerHTML);
                 video.remove();
             });
-    }, []);
+    }, [className]);
     return (
         <video muted loop playsInline poster='/CVF_static_icon.webp' className={className} id='logo-mp4' >
             <source src="/CVF_motion.mp4" type="video/mp4" />
