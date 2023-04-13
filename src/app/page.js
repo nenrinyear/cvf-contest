346d575644c253f665a9220995ea8776cc36af16
年輪 <nenrin.yearring@gmail.com>
2023-04-14 03:24:01 +0900

    wip: redesign by tmp text

--- src/app/page.js
+++ src/app/page.js
@@ -22,33 +22,64 @@ const zen_maru_gothic = Zen_Maru_Gothic({
     display: 'swap',
 })
 
+export function Header() {
+    return (
+        <div className={styles.header}>
+            <div className={styles.header_canvas_top}>
+                <CanvasAnimation className={styles.header_canvas_svg} />
+            </div>
+            <div className={styles.header_logo_top}>
+                <LogoMovieAnimation className={styles.header_logo_movie} />
+            </div>
+            <div className={styles.header_title_top}>
+                <p className={`${styles.header_title_slogan} ${jost.className}`}>
+                    Next to the Stage.
+                </p>
+                <p className={`${styles.header_title_CVF} ${jost.className}`}>
+                    CVF
+                </p>
+            </div>
+            <div className={`${styles.header_scroll_down_top} ${jost.className}`}>
+                <span className={styles.header_scroll_down_arrow}>&ang;</span>
+                <p className={styles.header_scroll_down_text}>Scroll Down</p>
+                
+            </div>
+        </div>
+    )
+}
+
 export default function Home() {
     return (
         <div className={`${styles.top} ${zen_maru_gothic.className}`}>
-            <header className={styles.header}>
-                <div className={styles.header_canvas_top}>
-                    <CanvasAnimation className={styles.header_canvas_svg} />
-                </div>
-                <div className={styles.header_logo_top}>
-                    <LogoMovieAnimation className={styles.header_logo_movie} />
-                </div>
-                <div className={styles.header_title_top}>
-                    <p className={`${styles.header_title_slogan} ${jost.className}`}>
-                        Next to the Stage.
-                    </p>
-                    <p className={`${styles.header_title_CVF} ${jost.className}`}>
-                        CVF
-                    </p>
-                </div>
-                <div className={`${styles.header_scroll_down_top} ${jost.className}`}>
-                    <span className={styles.header_scroll_down_arrow}>&ang;</span>
-                    <p className={styles.header_scroll_down_text}>Scroll Down</p>
-                    
-                </div>
-            </header>
             <NavCompornent />
+            <Header />
             <main>
-                <div className="tw-h-screen">aaaaaaaaaaaaaa</div>
+                <div className={styles.main}>
+                    <div className={styles.main_title}>
+                        <h1 className={`${styles.main_title_text} ${jost.className}`}>CVF</h1>
+                    </div>
+                    <div className={styles.main_content}>
+                        <p className={styles.main_content_text}>
+                            CVFは、
+                            <br />
+                            ファッションの世界における
+                            <br />
+                            デザイナー、モデル、ブランド、
+                            <br />
+                            エージェンシー、メディア、
+                            <br />
+                            ファッション関連の企業、
+                            <br />
+                            そしてファッションに関わる
+                            <br />
+                            すべての人々の
+                            <br />
+                            交流を促進するための
+                            <br />
+                            プラットフォームです。
+                        </p>
+                    </div>
+                </div>
             </main>
         </div>
     )
--- src/nav.js
+++ src/nav.js
@@ -25,56 +25,63 @@ export default function NavCompornent() {
             if (window.scrollY > 0) {
                 nav.classList.add(navStyles.nav_top_Fixed);
             } else {
-                nav.classList.remove(navStyles.nav_top_Fixed);
+                if (!navMenuOpen) {
+                    console.log(navMenuOpen);
+                    nav.classList.remove(navStyles.nav_top_Fixed);
+                }
             }
         };
         window.addEventListener('scroll', navShow);
         navShow();
-    }, []);
+        return () => window.removeEventListener('scroll', navShow);
+    }, [navMenuOpen,]);
     return (
-        <nav className={`${navStyles.nav_top} ${jost.className}`} id='nav'>
-            <Link
-                className={navStyles.nav_logo}
-                href="/"
-                >
-                <Image
-                    src="/CVF_static_icon.webp"
-                    alt="CVF"
-                    width={50}
-                    height={50}
-                    className={navStyles.nav_logo_image}
-                />
-                <p className={navStyles.nav_logo_text}>CVF</p>
-            </Link>
-            <ul className={navStyles.nav_list}>
-                <li className={navStyles.nav_item}>
-                    <Link href="/" className={navStyles.nav_link}>Home</Link>
-                </li>
-                <li className={navStyles.nav_item}>
-                    <Link href="/about" className={navStyles.nav_link}>About</Link>
-                </li>
-                <li className={navStyles.nav_item}>
-                    <Link href="/contact" className={navStyles.nav_link}>Contact</Link>
-                </li>
-            </ul>
-            <div className={`${navStyles.nav_humbger} ${navMenuOpen? navStyles.nav_humbger_Open : ''}`} onClick={navMenuOpenToggle}>
-                <div className={navStyles.nav_humbger_line}></div>
-                <div className={navStyles.nav_humbger_line}></div>
-                <div className={navStyles.nav_humbger_line}></div>
-            </div>
-            <div className={`${navStyles.nav_menu} ${navMenuOpen ? navStyles.nav_menu_Open : ''}`}>
-                <ul className={navStyles.nav_menu_list}>
-                    <li className={navStyles.nav_menu_item}>
-                        <Link href="/" className={navStyles.nav_menu_link}>Home</Link>
+        <header>
+            <nav className={`${navStyles.nav_top} ${jost.className}`} id='nav'>
+                <Link
+                    className={navStyles.nav_logo}
+                    href="/"
+                    >
+                    <Image
+                        src="/CVF_static_icon.webp"
+                        alt="CVF"
+                        width={50}
+                        height={50}
+                        className={navStyles.nav_logo_image}
+                    />
+                    <p className={navStyles.nav_logo_text}>CVF</p>
+                </Link>
+                <ul className={navStyles.nav_list}>
+                    <li className={navStyles.nav_item}>
+                        <Link href="/" className={navStyles.nav_link}>Home</Link>
                     </li>
-                    <li className={navStyles.nav_menu_item}>
-                        <Link href="/about" className={navStyles.nav_menu_link}>About</Link>
+                    <li className={navStyles.nav_item}>
+                        <Link href="/about" className={navStyles.nav_link}>About</Link>
                     </li>
-                    <li className={navStyles.nav_menu_item}>
-                        <Link href="/contact" className={navStyles.nav_menu_link}>Contact</Link>
+                    <li className={navStyles.nav_item}>
+                        <Link href="/contact" className={navStyles.nav_link}>Contact</Link>
                     </li>
                 </ul>
-            </div>
-        </nav>
+                <div className={`${navStyles.nav_humbger} ${navMenuOpen? navStyles.nav_humbger_Open : ''}`} onClick={navMenuOpenToggle}>
+                    <div className={navStyles.nav_humbger_line}></div>
+                    <div className={navStyles.nav_humbger_line}></div>
+                    <div className={navStyles.nav_humbger_line}></div>
+                </div>
+                <div className={`${navStyles.nav_menu} ${navMenuOpen ? navStyles.nav_menu_Open : ''}`}>
+                    <ul className={navStyles.nav_menu_list}>
+                        <li className={navStyles.nav_menu_item}>
+                            <Link href="/" className={navStyles.nav_menu_link}>Home</Link>
+                        </li>
+                        <li className={navStyles.nav_menu_item}>
+                            <Link href="/about" className={navStyles.nav_menu_link}>About</Link>
+                        </li>
+                        <li className={navStyles.nav_menu_item}>
+                            <Link href="/contact" className={navStyles.nav_menu_link}>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className={`${navStyles.nav_bg} ${navMenuOpen? navStyles.nav_bg_Open : ''}`}></div>
+            </nav>
+        </header>
     )
 }
