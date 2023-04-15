00be05c737f7e95f7036f43b9341586b502b7aa3
年輪 <nenrin.yearring@gmail.com>
2023-04-15 21:26:03 +0900

    add lottie react package

--- src/CanvasAnimation.js
+++ src/CanvasAnimation.js
@@ -1,22 +1,18 @@
 "use client";
-import Script from "next/script"
+import Script from "next/script";
+import Lottie from "lottie-react";
+
+import CVF_motion from "../public/logomotion/CVF_motion.json";
 
 export default function CanvasAnimation({className }) {
     return (
         <>
-            <div id="background-motion" className={className}></div>
-            <Script
-                src="/logomotion/lottie.min.js"
-                onLoad={() => {
-                    const animation = lottie.loadAnimation({
-                        container: document.getElementById('background-motion'),
-                        renderer: 'svg',
-                        loop: true,
-                        autoplay: true,
-                        path: '/logomotion/CVF_motion.json',
-                    });
-                }}
-            />
+            <Lottie 
+                animationData={CVF_motion}
+                loop={true}
+                autoplay={true}
+                renderer="svg"
+                />
         </>
     )
 }
--- src/nav.js
+++ src/nav.js
@@ -3,8 +3,37 @@ import Image from 'next/image';
 import navStyles from './nav.module.css';
 
 import { Jost } from 'next/font/google';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
+
+import { motion } from 'framer-motion';
+
+
+const navVariants = {
+    open: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            staggerChildren: 0.07,
+            delayChildren: 0.2,
+        },
+    },
+    closed: {
+        x: '100%',
+        opacity: 0,
+    },
+};
+
+const navItemVariants = {
+    open: {
+        opacity: 1,
+        x: 0,
+    },
+    closed: {
+        opacity: 0,
+        x: '10%',
+    },
+};
 
 const jost = Jost({
     subsets: ['latin'],
@@ -12,6 +41,25 @@ const jost = Jost({
     weights: [200, 300, 500],
     display: 'swap',
 })
+
+const Links = [
+    {
+        href: '/',
+        text: 'Home',
+    },
+    {
+        href: '/about',
+        text: 'About',
+    },
+    {
+        href: '/contact',
+        text: 'Contact',
+    },
+    {
+        href: 'https://mouri.work',
+        text: 'M0UR1 Works',
+    }
+];
 
 export default function NavCompornent() {
     const [navMenuOpen, setNavMenuOpen] = useState(false);
@@ -35,6 +83,7 @@ export default function NavCompornent() {
         window.addEventListener('scroll', navShow);
         return () => window.removeEventListener('scroll', navShow);
     }, [navMenuOpen,]);
+    const containerRef = useRef(null);
     return (
         <header className={navStyles.nav}>
             <nav className={`${navStyles.nav_top} ${jost.className}`} id='nav'>
@@ -52,35 +101,46 @@ export default function NavCompornent() {
                     <p className={navStyles.nav_logo_text}>CVF</p>
                 </Link>
                 <ul className={navStyles.nav_list}>
-                    <li className={navStyles.nav_item}>
-                        <Link href="/" className={navStyles.nav_link}>Home</Link>
-                    </li>
-                    <li className={navStyles.nav_item}>
-                        <Link href="/about" className={navStyles.nav_link}>About</Link>
-                    </li>
-                    <li className={navStyles.nav_item}>
-                        <Link href="/contact" className={navStyles.nav_link}>Contact</Link>
-                    </li>
+                    {Links.map((link) => (
+                        <li className={navStyles.nav_item} key={link.href}>
+                            <Link 
+                                href={link.href}
+                                className={navStyles.nav_link}
+                            >
+                                {link.text}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
                 <div className={`${navStyles.nav_humbger} ${navMenuOpen? navStyles.nav_humbger_Open : ''}`} onClick={navMenuOpenToggle}>
                     <div className={navStyles.nav_humbger_line}></div>
                     <div className={navStyles.nav_humbger_line}></div>
                     <div className={navStyles.nav_humbger_line}></div>
                 </div>
-                <div className={`${navStyles.nav_menu} ${navMenuOpen ? navStyles.nav_menu_Open : ''}`}>
+                <motion.div
+                    initial={false}
+                    animate={navMenuOpen ? 'open' : 'closed'}
+                    variants={navVariants}
+                    ref={containerRef}
+                    className={`${navStyles.nav_menu}`}>
                     <ul className={navStyles.nav_menu_list}>
-                        <li className={navStyles.nav_menu_item}>
-                            <Link href="/" className={navStyles.nav_menu_link}>Home</Link>
-                        </li>
-                        <li className={navStyles.nav_menu_item}>
-                            <Link href="/about" className={navStyles.nav_menu_link}>About</Link>
-                        </li>
-                        <li className={navStyles.nav_menu_item}>
-                            <Link href="/contact" className={navStyles.nav_menu_link}>Contact</Link>
-                        </li>
+                        {Links.map((link) => (
+                            <motion.li
+                                variants={navItemVariants}
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.95 }}
+                                className={navStyles.nav_menu_item} key={link.href}>
+                                <Link
+                                    href={link.href}
+                                    className={navStyles.nav_menu_link}
+                                >
+                                    {link.text}
+                                </Link>
+                            </motion.li>
+                        ))}
                     </ul>
-                </div>
-                <div className={`${navStyles.nav_bg} ${navMenuOpen? navStyles.nav_bg_Open : ''}`}></div>
+                </motion.div>
+                <div className={`${navStyles.nav_bg} ${navMenuOpen? navStyles.nav_bg_Open: undefined}`} />
             </nav>
         </header>
     )
