0f2abb1cd92c07dd4a2d6e60ba19a746e8339dd7
年輪 <nenrin.yearring@gmail.com>
2023-04-19 02:28:45 +0900

    update about page

--- src/app/About.js
+++ src/app/About.js
@@ -8,36 +8,50 @@ const jost = Jost({
     weights: [200, 300, 500],
     display: 'swap',
 });
-const zen_maru_gothic_300 = Zen_Maru_Gothic({
+const zen_maru_gothic_400 = Zen_Maru_Gothic({
     subsets: ['latin'],
     style: ['normal'],
-    weight: "300",
+    weight: "400",
     display: 'swap',
 })
 
 export default function About() {
     return (
         <div className={styles.top}>
-            <div className={styles.Title}>
-                <p className={`${styles.Title_text} ${jost.className}`}>
-                    What&#x27;s CVF?
-                    <span className={`${styles.Title_text_ja}`}>
-                        CVFとは？
-                    </span>
-                </p>
-            </div>
-            <div className={styles.About}>
-                <p className={`${styles.About_text} ${jost.className}`}>
-                    CVFは、音楽が大好きな人たちが集まって、音楽を共有するコミュニティです。
-                </p>
-                <div className={styles.GoToAboutPage}>
-                    <p className={`${styles.GoToAboutPage_text} ${jost.className}`}>
-                        <Link
-                            href="/about"
-                            className={styles.GoToAboutPage_text_link}>
-                            CVFについてもっと知る
-                        </Link>
+            <div className={styles.Layout}>
+                <div className={styles.Title}>
+                    <p className={`${styles.Title_text} ${jost.className}`}>
+                        What&#x27;s CVF?
+                        <span className={`${styles.Title_text_ja}`}>
+                            CVFとは？
+                        </span>
                     </p>
+                </div>
+                <div className={styles.About}>
+                    <p className={`${styles.About_text} ${zen_maru_gothic_400.className}`}>
+                        CVFは、音楽が大好きな人たちが集まって、音楽を共有するコミュニティです。
+                        <br />
+                        このコミュニティでは、音楽を通して、人々がつながり、新しい発見をし、
+                        <br />
+                        そして、それらを共有することができます。
+                        <br />
+                        このコミュニティに参加することで、音楽を通して、
+                        <br />
+                        人々がつながり、新しい発見をし、そして、それらを共有することができます。
+                        <br />
+                        このコミュニティに参加することで、音楽を通して、
+                        <br />
+                        人々がつながり、新しい発見をし、そして、それらを共有することができます。
+                    </p>
+                    <div className={styles.GoToAboutPage}>
+                        <p className={`${styles.GoToAboutPage_text} ${jost.className}`}>
+                            <Link
+                                href="/about"
+                                className={styles.GoToAboutPage_text_link}>
+                                CVFについてもっと知る
+                            </Link>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
