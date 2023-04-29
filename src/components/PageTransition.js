0649f141424ebb1148b792e397e95cc3f4da7844
年輪 <nenrin.yearring@gmail.com>
2023-04-30 01:13:57 +0900

    update page transition motion

--- src/components/PageTransition.js
+++ src/components/PageTransition.js
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 export default function PageTransition({ children }) {
     return (
         <motion.div
-            initial={{ y: 20, opacity: 0 }}
+            initial={{ y: 20, opacity: 0.01 }}
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: 20, opacity: 0 }}
             transition={{
