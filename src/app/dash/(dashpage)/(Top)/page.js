8b56517aa7f736d359a202c85a0994cbb2280938
年輪 <nenrin.yearring@gmail.com>
2023-04-30 17:47:44 +0900

    wip: update dashboard page

--- src/app/dash/(dashpage)/(Top)/page.js
+++ src/app/dash/(dashpage)/(Top)/page.js
@@ -3,7 +3,10 @@
 import PageTransition from '@/components/PageTransition';
 import styles from './page.module.css';
 
+import { useSession } from 'next-auth/react';
+
 export default function Dashboard() {
+    const { data: session, status } = useSession();
     return (
         <PageTransition>
             <div className={styles.Title}>
@@ -13,7 +16,19 @@ export default function Dashboard() {
             </div>
             <div className={styles.About}>
                 <p className={`${styles.About_text}`}>
-                    ここにマイページの内容が入ります。
+                    ようこそ、{session.user.userData.name}さん。
+                </p>
+                <p className={`${styles.About_text}`}>
+                    現在調整中です。登録されている情報は以下の通りです。
+                </p>
+                <p className={`${styles.About_text}`}>
+                    ユーザー名: {session.user.userData.name}
+                </p>
+                <p className={`${styles.About_text}`}>
+                    TwitterID: {session.user.userData.twitterID}
+                </p>
+                <p className={`${styles.About_text}`}>
+                    登録された日時: {session.user.userData.createdAt}
                 </p>
             </div>
         </PageTransition>
